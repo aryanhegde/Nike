4f16d40b7f2a467357b88454f15522e2c5dc69c0
Aaryan Hegde <hegdearyan000@gmail.com>
2023-08-26 20:45:11 +0530

    edit 1

--- src/sections/SpecialOffers.jsx
+++ src/sections/SpecialOffers.jsx
@@ -5,13 +5,13 @@ const SpecialOffer = () => {
   return (
     <section className="flex justify-between items-center max-xl:flex-col-reverse gap-10 max-container">
       <div className="flex-1">
-        <img
+        {/* <img
           src={""}
           alt="Shoe Promotion"
           width={773}
           height={687}
           className="object-contain w-full"
-        />
+        /> */}
       </div>
       <div className="flex flex-1 flex-col">
         <h2 className="text-4xl font-palanquin font-bold">
